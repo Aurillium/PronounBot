55774207e7d3f0d632825d7b12dfb4ebbca8c1e3
Memphis Marshall <memphism2003@gmail.com>
2022-06-09 00:48:52 +0800

    Unregister test commands on shutdown

--- main.js
+++ main.js
@@ -15,16 +15,20 @@ for (const file of commandFiles) {
 }
 
 const rest = new REST({ version: '9' }).setToken(token);
+const registered = [];
 
 (async () => {
 	try {
 		console.log('Started refreshing slash commands.');
 
 		if (testing_mode) {
-			await rest.put(	
+			let registered_commands = await rest.put(	
 				Routes.applicationGuildCommands(client_id, testing_guild),
 				{ body: commands },
 			);
+			registered_commands.forEach(command => {
+				registered.push(command.id);
+			});
 		} else {
 			await rest.put(
 				Routes.applicationCommands(client_id),
@@ -48,6 +52,31 @@ client.on('ready', () => {
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 	await responses[interaction.commandName](interaction);
+});
+
+async function onExit() {
+	console.log("\nExitting...");
+	if (testing_mode) {
+		let guild = await client.guilds.fetch(testing_guild);
+		for (let i = 0; i < registered.length; i++) {
+			const id = registered[i];
+			let command = await guild.commands.fetch(id);
+			command.delete();
+			console.log(`- Unregistered '${command.name}'.`);
+		}
+		console.log("Unregistered all commands; Exitting now.");
+	} else {
+		console.log("Production mode does not unregister commands; Exitting now.");
+	}
+}
+
+process.on("SIGINT", () => {
+	onExit().then(result => {
+		process.exit();
+	}).catch(error => {
+		console.log(error);
+		process.exit();
+	});
 });
 
 // https://discord.com/api/oauth2/authorize?client_id=983907393823969312&permissions=2147483648&scope=bot
